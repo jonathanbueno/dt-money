d7fc175bbff351eac2c694ed1ce6c5c757a9cdad
Jonathan Bueno <jfb1601@hotmail.com>
2022-01-27 21:45:15 +0000

    Initial structure for New Transaction's modal passing props from App to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenModal: () => void;
+}
+
+export function Header({ onOpenModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt='dt money' />
-        <button>Nova transação</button>
+        <button onClick={onOpenModal}>Nova transação</button>
       </Content>
     </Container>
   );
